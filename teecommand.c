8420e559562d34b3b22e0752ec8b823f10981801
sushithbalu <sushith1132@gmail.com>
2014-09-24 00:22:38 +0530

    tee command

--- teecommand.c
+++ teecommand.c
@@ -1,11 +1,31 @@
-#include<sys/type.h>
+/*usage: ./a.out  <existing file>  <new or existing file>*/
+
+#include<sys/types.h>
 #include<sys/stat.h>
 #include<fcntl.h>
 #include<stdio.h>
-
-int main()
+#include<assert.h>
+#include<stdlib.h>
+int main(int argc, char **argv)
 {
-	int fd;
-	fd = open()
+	int fdr,  fdw;
+	size_t nbr, nbw;
+	char *buf;
+
+	fdr = open(argv[1], O_RDONLY);
+	assert(fdr > 0);
+	fdw = open(argv[2], O_WRONLY|O_CREAT|O_TRUNC, 0644);
+	assert(fdw > 0);
+	while((nbr = read(fdr, buf, sizeof(buf))) > 0){
+		write(1, buf, nbr);
+		nbw = write(fdw, buf, nbr);
+		if(nbw != nbr){
+			exit(1);
+		}
+	}
+	if(nbr == -1){
+		fprintf(stderr, "write error");
+		exit(1);
+	}
 	return 0;
 }
